36b5ec15be6663c24b235da43d9e1d1816a04cf9
iminsik <iminsik@gmail.com>
2017-03-03 22:08:11 -0800

    ignore *.swp in git and update text

---
+++ src/components/SongList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SongList extends Component {
+	render() {
+		return (
+			<div>
+				My Song List - Yellow!!
+			</div>
+		)
+	}
+}
+
+export default SongList;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/Main';
+//import App from './components/Main';
+import SongList from './components/SongList';
 
 //./components/Main.js
 
 // Render the main component into the dom
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<SongList />, document.getElementById('app'));

8ae076c621fb067d51a547dd813a67dde5db2387
iminsik <iminsik@gmail.com>
2017-03-03 21:02:54 -0800

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import 'core-js/fn/object/assign';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/Main';
+
+//./components/Main.js
+
+// Render the main component into the dom
+ReactDOM.render(<App />, document.getElementById('app'));
